cfb5d09aabeebfbb10d9e6858964ffd760fe914f
Jeremy Judeaux <jeremy.judeaux@EF.com>
2017-05-22 17:50:47 +0800

    Use proxy for bind+promisify operator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 const OPERATOR = Symbol('callback-to-promise-operator');
+const NO_PROXY_OPERATOR = Symbol('callback-to-promise-operator-no-proxy');
 const RETURNED_VALUE = Symbol('callback-to-promise-returned-value');
 const THROWN_VALUE = Symbol('callback-to-promise-thrown-value');
 const CALLBACK_ARGUMENTS = Symbol('callback-to-promise-callback-arguments');
+const CONTEXT = Symbol('callback-to-promise-proxy-context');
 
-function callbackToPromiseOperator(...args) {
+const defineFunctionProperties = (fakeFunction, actualFunction) =>
+  Object.defineProperties(fakeFunction, {
+    displayName: {
+      configurable: true,
+      enumerable: false,
+      writable: false,
+      value: actualFunction.displayName,
+    },
+    name: {
+      configurable: true,
+      enumerable: false,
+      writable: false,
+      value: actualFunction.name,
+    },
+    length: {
+      configurable: true,
+      enumerable: false,
+      writable: false,
+      value: actualFunction.length,
+    },
+  });
+
+const promisifyFunction = (func, context) => (...args) => {
   let resolve;
   let reject;
   const promise = new Promise((resolveParam, rejectParam) => {
@@ -11,7 +35,7 @@ function callbackToPromiseOperator(...args) {
     reject = rejectParam;
   });
   try {
-    promise[RETURNED_VALUE] = this.call(undefined, ...args, (...callbackArgs) => {
+    promise[RETURNED_VALUE] = func.call(context, ...args, (...callbackArgs) => {
       promise[CALLBACK_ARGUMENTS] = callbackArgs;
       if (callbackArgs[0]) {
         reject(callbackArgs[0]);
@@ -24,17 +48,82 @@ function callbackToPromiseOperator(...args) {
     reject(exception);
   }
   return promise;
+};
+
+const noProxyGetter = {
+  configurable: false,
+  enumerable: false,
+  get() {
+    const promisified = promisifyFunction(this, undefined);
+    defineFunctionProperties(promisified, this);
+    return promisified;
+  },
+};
+
+if (typeof Proxy !== 'undefined') {
+  const proxyHandler = {
+    get(target, key) {
+      const value = target[key];
+      if (typeof value === 'function') {
+        const promisified = promisifyFunction(value, target);
+        defineFunctionProperties(promisified, value);
+        return promisified;
+      }
+      return value;
+    },
+  };
+  const functionProxyHandler = {
+    get(target, key) {
+      const context = target[CONTEXT];
+      const value = context[key];
+      if (typeof value === 'function') {
+        const promisified = promisifyFunction(value, context);
+        defineFunctionProperties(promisified, value);
+        return promisified;
+      }
+      return value;
+    },
+    has(target, key) {
+      const context = target[CONTEXT];
+      return key in context;
+    },
+  };
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Object.prototype, OPERATOR, {
+    configurable: false,
+    enumerable: false,
+    get() {
+      let boundOperator;
+      if (typeof this === 'function') {
+        const target = promisifyFunction(this, undefined);
+        defineFunctionProperties(target, this);
+        target[CONTEXT] = this;
+        boundOperator = new Proxy(target, functionProxyHandler);
+      } else {
+        boundOperator = new Proxy(this, proxyHandler);
+      }
+      if (typeof this === 'object' || typeof this === 'function') {
+        // eslint-disable-next-line no-extend-native
+        Object.defineProperty(this, OPERATOR, {
+          configurable: false,
+          enumerable: false,
+          writable: false,
+          value: boundOperator,
+        });
+      }
+      return boundOperator;
+    },
+  });
+} else {
+  // eslint-disable-next-line no-extend-native
+  Object.defineProperty(Function.prototype, OPERATOR, noProxyGetter);
 }
 
 // eslint-disable-next-line no-extend-native
-Object.defineProperty(Object.prototype, OPERATOR, {
-  configurable: false,
-  enumerable: false,
-  writable: false,
-  value: callbackToPromiseOperator,
-});
+Object.defineProperty(Function.prototype, NO_PROXY_OPERATOR, noProxyGetter);
 
 export default OPERATOR;
+export const noproxy = NO_PROXY_OPERATOR;
 export {
   CALLBACK_ARGUMENTS,
   RETURNED_VALUE,
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 import expect from 'must';
-import Ψ, { CALLBACK_ARGUMENTS, RETURNED_VALUE, THROWN_VALUE } from '../src/index';
+import withproxy, { noproxy, CALLBACK_ARGUMENTS, RETURNED_VALUE, THROWN_VALUE } from '../src/index';
 
 const SOME_ARG = { isSomeArg: true };
 const SOME_CALLBACK_ARGS = [null, 1, 2, 3];
@@ -9,12 +9,12 @@ const SOME_THROWN_EXCEPTION = { isSomeThrownException: true };
 const SOME_RESULT = { isSomeResult: true };
 const SOME_ERROR = { isSomeError: true };
 
-describe('callback-to-promise-operator', () => {
+const describeTests = (operator) => {
   it('resolves on sync callback result', () => {
     const asyncFunc = (callback) => {
       callback(null, SOME_RESULT);
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.resolve.to.equal(SOME_RESULT);
   });
@@ -23,7 +23,7 @@ describe('callback-to-promise-operator', () => {
     const asyncFunc = (callback) => {
       setTimeout(() => callback(null, SOME_RESULT), 0);
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.resolve.to.equal(SOME_RESULT);
   });
@@ -32,7 +32,7 @@ describe('callback-to-promise-operator', () => {
     const asyncFunc = () => {
       throw SOME_THROWN_EXCEPTION;
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.reject.to.equal(SOME_THROWN_EXCEPTION);
   });
@@ -42,7 +42,7 @@ describe('callback-to-promise-operator', () => {
     const asyncFunc = (callback) => {
       callback(SOME_ERROR);
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.reject.to.equal(SOME_ERROR);
   });
@@ -51,7 +51,7 @@ describe('callback-to-promise-operator', () => {
     const asyncFunc = (callback) => {
       setTimeout(() => callback(SOME_ERROR), 0);
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.reject.to.equal(SOME_ERROR);
   });
@@ -62,7 +62,7 @@ describe('callback-to-promise-operator', () => {
       expect(callback).to.be.a.function();
       callback(null, true);
     };
-    const promise = asyncFunc[Ψ](SOME_ARG);
+    const promise = asyncFunc[operator](SOME_ARG);
     expect(promise).to.be.a(Promise);
     return expect(promise).to.resolve.to.be.true();
   });
@@ -71,7 +71,7 @@ describe('callback-to-promise-operator', () => {
     const asyncFunc = (callback) => {
       callback(...SOME_CALLBACK_ARGS);
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     return expect(promise).to.resolve.to.equal(SOME_CALLBACK_ARGS[1])
       .then(() => {
@@ -84,7 +84,7 @@ describe('callback-to-promise-operator', () => {
       callback(null, true);
       return SOME_RETURNED_VALUE;
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     expect(promise[RETURNED_VALUE]).to.equal(SOME_RETURNED_VALUE);
     return expect(promise).to.resolve.to.be.true();
@@ -95,9 +95,56 @@ describe('callback-to-promise-operator', () => {
       callback(null, true);
       throw SOME_THROWN_EXCEPTION;
     };
-    const promise = asyncFunc[Ψ]();
+    const promise = asyncFunc[operator]();
     expect(promise).to.be.a(Promise);
     expect(promise[THROWN_VALUE]).to.equal(SOME_THROWN_EXCEPTION);
     return expect(promise).to.resolve.to.be.true();
+  });
+
+  it('forwards function name and arguments length', () => {
+    // eslint-disable-next-line no-unused-vars
+    function someFunction(arg1, arg2) {
+    }
+
+    const promisified = someFunction[operator];
+    expect(promisified()).to.be.a(Promise);
+    expect(promisified.name).to.equal(someFunction.name);
+    expect(promisified.displayName).to.equal(someFunction.displayName);
+    expect(promisified.length).to.equal(someFunction.length);
+  });
+};
+
+describe('callback-to-promise-operator', () => {
+  describe('with proxy', () => {
+    describeTests(withproxy);
+
+    it('promisified "." member with expected context', () => {
+      const object = {
+        asyncFunc(callback) {
+          const context = this;
+          setTimeout(() => callback(null, context), 0);
+        },
+      };
+      const promise = object[withproxy].asyncFunc();
+      expect(promise).to.be.a(Promise);
+      return expect(promise).to.resolve.to.equal(object);
+    });
+
+    it('promisified "[]" member with expected context', () => {
+      const symbol = Symbol('symbol-key');
+      const object = {
+        [symbol](callback) {
+          const context = this;
+          setTimeout(() => callback(null, context), 0);
+        },
+      };
+      const promise = object[withproxy][symbol]();
+      expect(promise).to.be.a(Promise);
+      return expect(promise).to.resolve.to.equal(object);
+    });
+  });
+
+  describe('without proxy', () => {
+    describeTests(noproxy);
   });
 });
